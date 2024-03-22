b14c2d8161fc6318da9fd0b747ca0011a8547058
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-03-22 22:56:45 +0500

    push ticket checker code

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Ticket-Fair-Challenge" 
---
+++ index.ts
@@ -0,0 +1,20 @@
+let customer_going_out_of_city = true;
+let customer_Is_student = true;
+let age = 38;
+
+if (age < 2) {
+  console.log("Free Ticket for infants");
+} 
+else if (customer_going_out_of_city && (age >= 2 && age < 5)) {
+  console.log("You have to buy tickets at half price of the actual ticket");
+} 
+else if (customer_going_out_of_city && customer_Is_student && (age >= 5 && age < 12)) {
+  console.log("50% off on ticket for all students");
+} 
+else if (customer_going_out_of_city && (age >= 5 && age < 65)) {
+  console.log("You can buy a full ticket");
+} 
+else if (customer_going_out_of_city || age >= 65) {
+  console.log("You can pay half price by showing your NIC if you are a senior citizen");
+}
+//Auhtor- HUMA MOHSIN
